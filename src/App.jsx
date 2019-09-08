aac467524a2407e82b91259716699b4bffffcc1a
Emmanuel Daniel <emmanuel.daniel@andela.com>
2019-09-08 23:33:02 +0100

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,6 @@
 import React from 'react';
-import './App.css';
-import Modal from './components/Modal/Modal';
-import {
-  FormInput,
-  FormOptions,
-  FormButton
-} from './components/FormFields/FormFields';
-
-const Mentor = () => {
-  return <h1>Hellow world</h1>;
-};
 function App() {
-  const options = [
-    {
-      att: {
-        value: 'm',
-        desc: 'Male'
-      }
-    },
-    {
-      att: {
-        value: 'f',
-        desc: 'Female'
-      }
-    }
-  ];
-  return (
-    <div className="">
-      <Modal title="Box Title">
-        <Mentor />
-        <FormInput id="name" title="First Name" value="none" />
-        <FormInput id="name" title="Name" value="none" noLabel="true" />
-        <FormButton id="name" title="Name" />
-        <FormOptions id="gender" title="Gender" options={options} />
-      </Modal>
-    </div>
-  );
+  return <h1> Hello Would ypu</h1>;
 }
 
 export default App;
